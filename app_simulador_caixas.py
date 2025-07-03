8f1d0d221f37f9a19b8d6e7a0a2e05eb0cf24039
Ânderson Matheus Flores De Oliveira <90271653+MySpaceCrazy@users.noreply.github.com>
2025-07-03 13:19:06 -0300

    Update app_simulador_caixas.py

--- app_simulador_caixas.py
+++ app_simulador_caixas.py
@@ -73,9 +73,9 @@ def empacotar_3d(df_base, df_mestre, comprimento_caixa, largura_caixa, altura_ca
         if qtd <= 0:
             continue  # Ignora produtos sem quantidade
 
-        if qtd > 10000:
-            st.warning(f"⚠️ Produto {row['ID_Produto']} com quantidade muito alta ({qtd}). Limitando para 10000.")
-            qtd = 10000
+        if qtd > 30000:
+            st.warning(f"⚠️ Produto {row['ID_Produto']} com quantidade muito alta ({qtd}). Limitando para 30000.")
+            qtd = 30000
 
         volume_un = (row["Comprimento"] * row["Largura"] * row["Altura"]) / 1000
         peso_bruto = row.get("Peso bruto", 0) or 0
